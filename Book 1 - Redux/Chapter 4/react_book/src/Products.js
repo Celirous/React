919419fd4a8ff6ab2555e638eaa081fed298bc7f
Celirous <aj.bellringer@gmail.com>
2026-03-05 11:36:19 +0200

    main

---
+++ Book 1 - Redux/Chapter 4/react_book/src/Products.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Products extends Component {
+  render() {
+    const products = ["Learning React", "Pro React", "Beginning React"];
+    const listProducts = products.map((product) => ( // the .map is what is basically looping out the info we have inside the class product in the Products.js file, so that it can desplay all the info we have 
+      <li key={product.toString()}>{product}</li>
+    ));
+    return (
+      <div>
+        <ul>{listProducts} </ul>
+      </div>
+    );
+  }
+}
+
+export default Products;
