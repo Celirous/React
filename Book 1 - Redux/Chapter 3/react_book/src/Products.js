09ac5445279642e87826774ece1c2d63e396cf21
Celirous <aj.bellringer@gmail.com>
2026-03-16 09:59:40 +0200

    Chapter 3 edit and Chapter 10

--- Book 1 - Redux/Chapter 3/react_book/src/Products.js
+++ Book 1 - Redux/Chapter 3/react_book/src/Products.js
@@ -4,7 +4,7 @@ class Products extends Component {
   render() {
     const products = ["Learning React", "Pro React", "Beginning React"];
     const listProducts = products.map((product) => ( // the .map is what is basically looping out the info we have inside the class product in the Products.js file, so that it can desplay all the info we have 
-      <li key={product.toString()}>{product}</li>
+      <li key={product.toString()}>{product}</li> 
     ));
     return (
       <div>
